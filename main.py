0e5b2574241a77ac9db58b269894f9f1c28aa584
Clevelandrocks10 <108141722+Clevelandrocks10@users.noreply.github.com>
2022-12-19 20:43:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from discord.ext import commands
 #        Passel so that all pinned messages get sent to the pins archive channel.
 
 # TODO change command here if you want to use another command, replace p. with anything you want inside the single ('') quotes
-client = commands.Bot(command_prefix='p.',
+client = commands.Bot(command_prefix='=',
                       status='Online', case_insensitive=True)
 client.remove_command("help")
 
@@ -39,11 +39,11 @@ mode = 1
 # TODO 
 # sendall is set to 0 by default, change to 1 if you want
 # the bot to send all pinned messages to the pins channel
-sendall = 0
+sendall = 1
 
 # TODO 
 # replace the 0 with the pins channel ID for your sever
-pins_channel = 948375476685111296/954794285222486046
+pins_channel = 1054568386333511690
 
 # TODO
 # add any black listed channel IDs as a list separated by a comma (,)
@@ -209,7 +209,7 @@ async def on_guild_channel_pins_update(channel, last_pin):
 
 
 # TODO Replace TOKEN with the token from discord developer portal 
-client.run('MTAwOTEyNjIzMTU4Njg5ODAyMg.GOPYKu.B3uxCiiU9IEOp3mWgoPe5Rq6b6w5Ynptc25Ews')
+#client.run('MTA1NDU2NDgxMDMxMTg2MDI4NA.GzksLM.jKl1MtfyIAa-W3qOMbMJhhpKu9UrKIVlAqMTX8')
 
 # TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
-#client.run(os.environ.get('TOKEN'))
+client.run(os.environ.get('TOKEN'))
